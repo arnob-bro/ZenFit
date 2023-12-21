c2743b693623e4f821f4ba3c27a27de615c379eb
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2023-12-21 17:50:56 +0600

    added exercise list

---
+++ lib/Exercise List/exercise_list.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/cupertino.dart';
+List<String>Triceps = [
+  'Bar Muscle-Up',
+  'Barbell Standing Tricep Extension',
+  'Bench Dip',
+  'Close Grip Bench Press',
+  'Close Grip Push Up',
+  'Dumbbell Lying Tricep Extension',
+  'Dumbbell Standing Tricep Extension',
+  'Overhead Cable Tricep Extension',
+  'Skull Crusher',
+  'Tricep Kickback',
+  'Tricep Pushdown with Bar',
+  'Tricep Pushdown With Rope',
+];
+List<String>Biceps = [
+  'Barbell Curl',
+  'Barbel Preacher Curl',
+  'Bodyweight Curls',
+  'Cable Curl With Bar',
+  'Cable Curl With Rope',
+  'Concentration Curl',
+  'Dumbbell Preacher Curl',
+  'EZ Barbell Bicep Curl',
+  'Hammer Curl',
+  'Incline Dumbbell Curl',
+  'Plate Hammer Curl',
+  'Seated Hammer Curl',
+  'Seated Dumbbell Curl',
+  'Spider Curl'
+];
+List<String>Abs = [
+  'Cable Crunch',
+  'Crunch',
+  'Hanging Knee Raise',
+  'Hanging Leg Raise',
+  'Hanging Sit-Up',
+  'Horizontal Wood Chop With Cable',
+  'Lying Leg Raise',
+  'Machine Crunch',
+  'Mountain Climbers',
+  'Oblique Crunch',
+  'Oblique Sit-Up',
+  'Plank',
+  'Russian Twist',
+  'Side Plank',
+  'Sit-Up',
+  'Vacuum Training'
+];
+
+List<String>Back = [
+  'Back Extension',
+  'Barbell Row',
+  'Barbell Shrug',
+  'Cable Close Grip Seated Row',
+  'Cable Wide Grip Seated Row',
+  'Chin-Up',
+  'Close Grip Pulldown',
+  'Deadlift',
+  'Dumbbell Deadlift',
+  'Dumbbell Row',
+  'Dumbbell Shrug',
+  'Good Morning',
+  'Lat Pulldown',
+  'Lat Pulldown With Supinated Grip',
+  'Machine Shrug',
+  'Pull Up',
+  'Seated Horizontal Machine Row',
+  'Seated Machine Row',
+  'T-Bar Row',
+  ''
+];
+
+List<String> Chest = [
+  'Bench Press',
+  'Cable Chest Press',
+  'Close Grip Push Up',
+  'Decline Bench Press',
+  'Dumbbell Chest Fly',
+  'Dumbbell Chest Press',
+  'Dumbbell Decline Chest Press',
+  'Incline Bench Press',
+  'Incline Dumbbell Press',
+  'Incline Push Up',
+  'Machine Chest Fly',
+  'Machine Horizontal Chest Press',
+  'Pec Deck Fly',
+  'Pullover',
+  'Push-Up',
+  'Smith Machine Bench Press',
+  'Smith Machine Incline Bench Press',
+  'Standing Cable Chest Fly'
+];
+
+List<String> Forearms = [
+  'Barbell Wrist Curl',
+  'Barbell Wrist Curl Behind The Back',
+  'Barbell Wrist Extension',
+  'Dumbbell Wrist Curl',
+  'Dumbbell Wrist Curl',
+  'Farmers Walk',
+  'One Hand Wrist Curl With Cable',
+  'One Hand Sidewise wrist Extension',
+  'Wrist Curl With Cable'
+];
+
+List<String> Glutes = [
+  'Barbell Lunge',
+  'Barbell Walking Lunges',
+  'Cable Glute Kickback',
+  'Dumbbell Lunge',
+  'Dumbbell Walking Lunge',
+  'Dumbbell Romanian Deadlift',
+  'Glute Bridge',
+  'Hip Adbuction With Machine',
+  'Hip Thrust',
+  'Hip Thrust Machine',
+  'Machine Glute Kickback',
+  'Romanian Deadlift',
+];
+List<String> Hamstrings = [
+  'Bulgarian Split Squat',
+  'Lying Leg Curl',
+  'Lying Hamstring Machine',
+  'Nordic Hamstring',
+  'Seated Leg Curl',
+  'Stiff-legged Deadlift',
+];
+List<String> Quads = [
+  'Hack Squat',
+  'Hack Squat Machine',
+  'Landmine Hack Squat',
+  'Landmine Squat',
+  'Leg Extension',
+  'Leg Press',
+  'Machine Forward Squat',
+  'Smith Machine Squat',
+  'Squat'
+];
+List<String> Shoulders = [
+  'Arnold Press',
+  'Barbell Upright Row',
+  'Cable Front Raise',
+  'Cable Lateral Raise',
+  'Cable Upright Row',
+  'Dumbbell Front Raise',
+  'Dumbbell Lateral Raise',
+  'Dumbbell Rear Delt Row',
+  'Dumbbel Shoulder Press',
+  'Face Pull',
+  'Handstand Push Up',
+  'Machine Lateral Raise',
+  'Machine Shoulder Press',
+  'Reverse Cable Fly',
+  'Reverse Dumbbell Fly',
+  'Reverse Machine Fly',
+  'Seated Lateral Raise',
+  'Seated Smith Machine Shoulder Press',
+  ''
+];
+List<String> Lowerlegs = [
+  'Seated Calf Raise',
+  'Standing Calf Raise'
+];
+
+List<String> cardio = [
+  'Running',
+  'Swimming',
+  'Cycling',
+  'Walking',
+  'Rowing',
+  'Cross Trainer',
+  'Jump Rope'
+];
